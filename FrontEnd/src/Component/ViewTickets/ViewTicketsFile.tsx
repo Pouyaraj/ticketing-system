7ad5130c0f78fff0a56989c01659bf3f9d6a8157
Pouyaraj <Pouyaraj74@gmail.com>
2024-12-19 15:12:26 -0600

    css added

--- FrontEnd/src/Component/ViewTickets/ViewTicketsFile.tsx
+++ FrontEnd/src/Component/ViewTickets/ViewTicketsFile.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import "./ViewTicketsFile.css"
 // Define the interface for the ticket data
 interface Ticket {
   id: number;
@@ -9,41 +9,41 @@ interface Ticket {
 }
 
 function ViewTicketsFile() {
-  // State to hold the list of tickets
+ 
   const [tickets, setTickets] = useState<Ticket[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [load, setLoad] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchTickets = async () => {
       try {
-        const response = await fetch('http://localhost:8080/tickets/get-tickets'); // Replace with your backend URL
+        const response = await fetch('http://localhost:8080/tickets/get-tickets'); 
         if (!response.ok) {
           throw new Error('Failed to fetch tickets');
         }
 
-        const data: Ticket[] = await response.json(); // Parse the response data
-        setTickets(data); // Update tickets state with the fetched data
+        const data: Ticket[] = await response.json(); 
+        setTickets(data);
       } catch (err) {
         setError('Failed to fetch tickets');
         console.error(err);
       } finally {
-        setLoading(false); // Hide loading after fetching
+        setLoad(false); 
       }
     };
 
     fetchTickets();
-  }, []); // Empty dependency array to run only once when component mounts
+  }, []); 
 
   return (
     <div>
       <h1>View Tickets</h1>
 
-      {loading && <p>Loading tickets...</p>}
+      {load}
       {error && <p>{error}</p>}
 
-      {!loading && !error && (
-        <table>
+      {!error && (
+        <table className='table'>
           <thead>
             <tr>
               <th>ID</th>
